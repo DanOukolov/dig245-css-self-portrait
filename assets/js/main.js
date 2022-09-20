63d04deff0ef2ec9509a8a92f95e5fe5aaf36486
DanOukolov <oukolovdaniel@gmail.com>
2022-09-20 00:00:01 -0400

    JS-updates
    Completed the JS challenges

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,20 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+function mousemove(event){
+  console.log("pageX: ",event.pageX, 
+  "pageY: ", event.pageY, 
+  "clientX: ", event.clientX, 
+  "clientY:", event.clientY)
+}
+
+window.addEventListener('mousemove', mousemove);
+
+
+var sumTops = document.querySelectorAll(".top");
+var sumBottoms = document.querySelectorAll('bottom');
+var sumRights = document.querySelectorAll('.right');
+var sumLefts = document.querySelectorAll('.left');
+var sumMiddles = document.querySelectorAll('.middle');
+var sumStandalones = document.querySelectorAll('.standalone');
+var sumElements = (sumTops + sumBottoms + sumRights + sumLefts + sumMiddles + sumMiddles + sumStandalones);
